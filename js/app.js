2c5f658b5bae84788c50415b28b2baa674984f54
Rick Selby <rick@selby-family.co.uk>
2022-07-21 21:32:16 +0100

    Override bootstrap variables

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
 
-window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
+// window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
+
+// import 'bootstrap/js/dist/alert';
+// import 'bootstrap/js/dist/button';
+import 'bootstrap/js/dist/carousel';
+// import 'bootstrap/js/dist/collapse';
+import 'bootstrap/js/dist/dropdown';
+// import 'bootstrap/js/dist/modal';
+// import 'bootstrap/js/dist/offcanvas';
+// import 'bootstrap/js/dist/popover';
+// import 'bootstrap/js/dist/scrollspy';
+// import 'bootstrap/js/dist/tab';
+// import 'bootstrap/js/dist/toast';
+// import 'bootstrap/js/dist/tooltip';
 
 import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
+// import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
