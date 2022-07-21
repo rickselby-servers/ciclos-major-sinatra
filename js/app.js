a4c643244eb8ccb8a0e63609a312d710d0f2c230
Rick Selby <rick@selby-family.co.uk>
2022-07-21 19:06:40 +0100

    Add fontawesome and finish off footer

---
+++ js/app.js
@@ -0,0 +1,7 @@
+
+window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
+
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
