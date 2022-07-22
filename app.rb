65618f045c7034b601ab08d6832e429aede6c231
Rick Selby <rick@selby-family.co.uk>
2022-07-22 22:20:34 +0100

    Use assets for cache busting the styles

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@
 
 require 'rubygems'
 require 'bundler'
+require 'logger'
 
 Bundler.require
 
@@ -11,6 +12,9 @@ configure do
   set :erb, escape_html: true
   set :show_exceptions, :after_handler if development?
   disable :dump_errors unless development?
+  ASSETS = JSON.parse(File.read('public/manifest.json'), symbolize_names: true)
+  LOGGER = Logger.new $stdout
+  $stdout.sync = true
   helpers Helpers
 end
 
