d7442b29068405a432c49218e6c3946cc1ed6f68
Rick Selby <rick@selby-family.co.uk>
2022-07-28 21:35:18 +0100

    Add Autumn 2023 camp booking pages

--- app.rb
+++ app.rb
@@ -54,7 +54,7 @@ get('/camps/cycling-and-triathlon') { erb :'camps/cycling_and_triathlon' }
 # Camps booking pages
 get('/camps/2022-autumn') { erb :'camps/2022-aut' }
 get('/camps/2023-spring') { erb :'camps/2023-spr' }
-get('/camps/2023-autumn') { erb :todo }
+get('/camps/2023-autumn') { erb :'camps/2023-aut' }
 get('/camps/2023-312') { erb :todo }
 # Hotels
 get('/hotels/can-ribera') { erb :'hotels/can_ribera' }
